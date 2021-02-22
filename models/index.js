44d9115306e92f943f12fd772b1199f64590142f
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-02-22 13:51:12 -0800

    add favicon middleware. install express-favicon

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 
+
 //path to config file
 
 const basename = path.basename(module.filename);
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const app = express();
+const favicon = require('express-favicon');
 //const angular;
 const routes  = require('./controllers/controller');
 const db = require('./models');
@@ -13,7 +14,7 @@ const db = require('./models');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
+app.use(favicon(__dirname + '/public/favicon.png'));
 
 // Angular Engine Here
 
