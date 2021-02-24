c255810b8be1a0063260d279585f509896e864ae
rasputinforever <erik.justin.portillo@gmail.com>
2021-02-23 19:48:09 -0800

    index and evernt get routes initiated

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,12 +2,27 @@ const express = require("express");
 const path = require('path');
 const router = express.Router();
 
+// need a path to the model
 
-// test setup
+// // GET routes HERE
+// index/splash screen
+router.get("/", function(req, res) {
+    
+});
 
-// router.get("/", function(req, res) {
+router.get("/characters/:id", function(req, res) {
 
+});
 
-// });
+// initial event
+router.get("/events", function(req, res) {
+
+});
+
+// index/splash screen
+router.get("/events/:id", function(req, res) {
+
+});
+
 
 //module.exports = router;
