25fe690add836494bda864868da0c3c1a8157501
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-02-25 11:57:31 -0800

    create association between encounters and options

--- models/options.js
+++ models/options.js
@@ -4,13 +4,15 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class options extends Model {
+
+    
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      options.belongsTo(models.encounters, {through: "events"});
     }
   };
   options.init({
