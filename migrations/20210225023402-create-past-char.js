d74901c94dcd6efff3488073aa5928b3f7c27758
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-02-27 11:42:54 -0800

    remove .env from gitignore. clean up migrations.

--- migrations/20210225023402-create-past-char.js
+++ migrations/20210225023402-create-past-char.js
@@ -1,4 +1,3 @@
-// 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('pastChars', {
@@ -24,7 +23,7 @@ module.exports = {
       },
     });
   },
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface) => {
     await queryInterface.dropTable('pastChars');
   },
 };
