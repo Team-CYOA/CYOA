f4a37c8d6fd703958721efd8ca7df240f83d7acb
Shawn Flanigan <shawnmflanigan@gmail.com>
2021-03-01 11:13:19 -0800

    remove console logs for animations

--- public/assets/js/encounterAnimation.js
+++ public/assets/js/encounterAnimation.js
@@ -1,21 +1,17 @@
-// const premEl = document.getElementById("demo")
-// const premiseText = premEl.innerText
-// let i = 0;
-// let speed = 20;
-// console.log('hello im alive')
-// function typeWriter(text) {
-//   // console.log(button.id);
-  
-//    if(i < premiseText.length) {
-//        premEl.innerHTML += premiseText.charAt(i);
-//        i++;
-//        console.log('text.chartAt', premiseText.charAt(i));
-//        setTimeout(typeWriter, speed);
-//    }
-// }
-// function getText () {
-//   premEl.innerText = ''
-//   console.log(premiseText);
-//   typeWriter(premiseText);
-// }
-// getText();
+const premEl = document.getElementById('demo');
+const premiseText = premEl.innerText;
+let i = 0;
+const speed = 20;
+function typeWriter(text) {
+
+  if (i < premiseText.length) {
+    premEl.innerHTML += premiseText.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+function getText() {
+  premEl.innerText = '';
+  typeWriter(premiseText);
+}
+getText();
