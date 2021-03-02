fada9d54394028d7af9fd12d9f3edcbb3cc4a48a
rasputinforever <erik.justin.portillo@gmail.com>
2021-03-02 12:51:42 -0800

    charID patched into "gotoNextenc" functions

--- public/assets/js/buttons.js
+++ public/assets/js/buttons.js
@@ -20,13 +20,33 @@ function ajaxOptions(optId) {
     consequenceText(response.consequence);
     document.getElementById('nextEnc').addEventListener('click', () => {
       // console.log(`next encounter is ${response.nextEncounter}`);
-      window.location = `/encounters/${response.nextEncounter}`;
+
+
+      // get characterID
+      const charEl = document.getElementsByClassName("charId")
+      console.log(charEl)
+      const charId = charEl[0].id
+      console.log(charId)
+
+
+      window.location = `/encounters/${response.nextEncounter}/${charId}`;
     });
   });
 }
 
 // generate next button that will grab the next encounter from db
 
+function updateCharEncounter() {
+  $.ajax({
+    url: ,
+    method: 'UPDATE'
+  }).then((res) => {
+    console.log("Character updated...")
+  })
+}
+
+
 // render the next encounters
 
 // fetch State
+
