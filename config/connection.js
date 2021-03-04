0e8a801cae9d5b1d5c0053275a316563d7f92bab
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-03-03 18:20:23 -0800

    change config

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const config = require('./config.json');
 
 let sequelize;
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize('mysql://ysz8apqfpluxcsbe:bbb276x5qxksz88c@z5zm8hebixwywy9d.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/f9ch08virkoqlbqs');
 } else {
   sequelize = new Sequelize(
     config.development.database,
